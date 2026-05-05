a2cf0fa73e2619c3d7f60c9116862c5cc473a409
jonec429 <j.carbungco@gmail.com>
2026-05-04 23:34:01 -0400

    FORCING update of Icons file

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -66,3 +66,6 @@ export const ChevronLeft = (p: any) => <Icon {...p}><path d="m15 18-6-6 6-6" /><
 export const Home = (p: any) => <Icon {...p}><path d="m3 9 9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" /><polyline points="9 22 9 12 15 12 15 22" /></Icon>;
 export const Menu = (p: any) => <Icon {...p}><line x1="4" y1="12" x2="20" y2="12" /><line x1="4" y1="6" x2="20" y2="6" /><line x1="4" y1="18" x2="20" y2="18" /></Icon>;
 export const User = (p: any) => <Icon {...p}><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" /><circle cx="12" cy="7" r="4" /></Icon>;
+export const Clipboard = (p: any) => <Icon {...p}><rect width="8" height="4" x="8" y="2" rx="1" ry="1"/><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"/></Icon>;
+export const GraduationCap = (p: any) => <Icon {...p}><path d="M22 10v6M2 10l10-5 10 5-10 5z"/><path d="M6 12v5c3 3 9 3 12 0v-5"/></Icon>;
+export const Mail = (p: any) => <Icon {...p}><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></Icon>;
